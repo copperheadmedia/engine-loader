364ba7e045599452c245cebee5e6f328a48fea0e
copperheadmedia <copperheadDJ@gmail.com>
2022-12-25 20:52:52 -0700

    fixed issue with json
    also fixed .gitignore

--- engine-loader.py
+++ engine-loader.py
@@ -1,7 +1,12 @@
 import tkinter as tk
+import config
 
 window = tk.Tk()
 
 window.title("engine loader")
+
+for i in range(len(config.engine_name)):
+    button = tk.Button(window, text=f"{config.engine_name[i]}",height=3, font=('Arial', 16))
+    button.pack()
 
 window.mainloop()
