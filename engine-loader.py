189d5c35531cf8f75dd4456077db67dcc36b4b13
copperheadmedia <copperheadDJ@gmail.com>
2022-12-25 21:33:29 -0700

    updated engine-loader.py
    allows it to write the correct output to the file

--- engine-loader.py
+++ engine-loader.py
@@ -1,9 +1,12 @@
 import tkinter as tk
 import config
+import os
 
 window = tk.Tk()
 
 window.title("engine loader")
+
+os.remove('engine-simulator/engine-sim-build_0_1_11a/assets/main.mr')
 
 def write_mods(file_name, folder_name):
     line1 = "import \"engine_sim.mr\""
@@ -15,6 +18,7 @@ def write_mods(file_name, folder_name):
         f.write(line1)
         f.write(line2)
         f.write("\n")
+        f.write(f"import \"engines/{folder_name}/{file_name}\"")
         f.write(line5)
         f.write(line6)
 
