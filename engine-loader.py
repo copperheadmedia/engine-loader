22de78f0e13a5159572062767637d0f85991bb1c
copperheadmedia <copperheadDJ@gmail.com>
2022-12-25 21:40:32 -0700

    updated engine-loader.py
    now allows you to write to the main.mr file

--- engine-loader.py
+++ engine-loader.py
@@ -6,21 +6,16 @@ window = tk.Tk()
 
 window.title("engine loader")
 
-os.remove('engine-simulator/engine-sim-build_0_1_11a/assets/main.mr')
+os.remove('engine_simulator/engine-sim-build_0_1_11a/assets/main.mr')
 
 def write_mods(file_name, folder_name):
-    line1 = "import \"engine_sim.mr\""
-    line2 = "import \"themes/default.mr\""
-    line5 = "use_default_theme()"
-    line6 = "main()"
-
-    with open('engine-simulator/engine-sim-build_0_1_11a/assets/main.mr', 'w') as f:
-        f.write(line1)
-        f.write(line2)
+    with open('engine_simulator/engine-sim-build_0_1_11a/assets/main.mr', 'w') as f:
+        f.write("import \"engine_sim.mr\"\n")
+        f.write("import \"themes/default.mr\"\n")
+        f.write(f"import \"engines/{folder_name}/{file_name}\"\n")
         f.write("\n")
-        f.write(f"import \"engines/{folder_name}/{file_name}\"")
-        f.write(line5)
-        f.write(line6)
+        f.write("use_default_theme()\n")
+        f.write("main()\n")
 
 for i in range(len(config.engine_name)):
     button = tk.Button(window, text=f"{config.engine_name[i]}",height=3, font=('Arial', 16), command=write_mods(config.file_name,config.folder_name))
